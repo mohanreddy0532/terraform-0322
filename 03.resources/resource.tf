c1b5564254bd0c07f5013bc69bdf620ff48d74ae
mohan <mohanreddy0532@gmail.com>
2022-03-18 17:38:58 +0530

    aws instance with SG

--- 03.resources/resource.tf
+++ 03.resources/resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami                    = "ami-091eb1485ca7182bd"
+  ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
