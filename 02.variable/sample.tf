24946b92edc3d4b5eee1fb4e432bf7c3940f1409
mohan <mohanreddy0532@gmail.com>
2022-03-18 06:52:38 +0530

    hello tf var desc.

--- 02.variable/sample.tf
+++ 02.variable/sample.tf
@@ -1,7 +1,19 @@
 variable "demo" {
+  type = string
   default = "Hello World"
 }
 
 output "Demo_var" {
   value = var.demo
+}
+
+variable "demo1" {
+  default = 100
+  type = number
+}
+
+variable "demo2" {
+  description = "Demo for Boolean demo"
+  default = true
+  type = bool
 }
